1325b21109be1574a8fcad91a7ad0ec56c5c2a4a
ace0507 <kysys0507@naver.com>
2022-05-13 08:55:16 +0900

    todayAlgorithm2

--- algorithm/직사각형 별찍기.js
+++ algorithm/직사각형 별찍기.js
@@ -26,3 +26,12 @@ process.stdin.on("data", (data) => {
     console.log(row);
   }
 });
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  const star = `${"*".repeat(a)}\n`;
+
+  console.log(star.repeat(b));
+});
