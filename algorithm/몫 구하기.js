a92f814a7b9b73787e7fb59c985a5dded5a98ba4
ace0507 <kysys0507@naver.com>
2022-11-22 12:01:30 +0900

    몫 구하기

---
+++ algorithm/몫 구하기.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+  let num = num1 / num2;
+
+  return Math.floor(num);
+}
+
+//Math.floor() : 소수점 이하를 버림한다.
+//Math.ceil() : 소수점 이하를 올림한다.
+//Math.round() : 소수점 이하를 반올림한다.
